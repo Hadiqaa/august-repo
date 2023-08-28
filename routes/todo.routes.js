c2b3758a73cdd1a099369bf77c31cd091f9a082c
Hadiqa Sumbal <57297404+Hadiqaa@users.noreply.github.com>
2023-08-28 12:05:44 +0500

    Delete routes directory

--- routes/todo.routes.js
+++
@@ -1,9 +0,0 @@
-//todo.routes.js
-const express = require('express');
-const router = express.Router();
-const todoController = require('../controllers/todo.controller');
-
-router.post('/', todoController.createNewTodoItem);
-router.get('/', todoController.getAllTodoItems);
-
-module.exports = router;
